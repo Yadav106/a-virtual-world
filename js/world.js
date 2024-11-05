f80820a73febdff6d32e211b71898ae29757bf71
Yadav106 <sahil.jp.yadav@gmail.com>
2024-11-05 14:33:39 +0530

    road generation done, working on procedural generation for buildings

--- js/world.js
+++ js/world.js
@@ -1,11 +1,21 @@
 class World {
-  constructor (graph, roadWidth = 100, roadRoundness = 3) {
+  constructor (graph, 
+    roadWidth = 100, 
+    roadRoundness = 10,
+    buildingWidth = 150,
+    buildingMinLength = 150,
+    spacing = 50
+  ) {
     this.graph = graph;
     this.roadWidth = roadWidth;
     this.roadRoundness = roadRoundness;
+    this.buildingWidth = buildingWidth;
+    this.buildingMinLength = buildingMinLength
+    this.spacing = spacing
 
     this.envelopes = [];
-    this.intersections = [];
+    this.roadBorders = [];
+    this.buildings = [];
 
     this.generate();
   }
@@ -19,12 +29,50 @@ class World {
       );
     }
 
-    Polygon.multiBreak(this.envelopes.map(e => e.poly));
+    this.roadBorders = Polygon.union(this.envelopes.map(e => e.poly));
+    this.buildings = this.#generateBuildings();
+  }
+
+  #generateBuildings() {
+    const tmpEnvelopes = [];
+    for (const seg of this.graph.segments) {
+      tmpEnvelopes.push(
+        new Envelope(
+          seg,
+          this.roadWidth + this.buildingWidth + this.spacing * 2,
+          this.roadRoundness
+        )
+      );
+    }
+
+    const guides = Polygon.union(tmpEnvelopes.map(e => e.poly));
+
+    for (let i = 0; i < guides.length; i++) {
+      const seg = guides[i];
+      if (sef.length() < this.buildingMinLength) {
+        guides.splice(i, 1);
+        i--;
+      }
+    }
+
+    return guides;
   }
 
   draw(ctx) {
     for (const env of this.envelopes) {
-      env.draw(ctx);
+      env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
+    }
+
+    for (const seg of this.graph.segments) {
+      seg.draw(ctx, { color: "white", width: 4, dash: [10, 10] });
+    }
+
+    for (const seg of this.roadBorders) {
+      seg.draw(ctx, { color: "white", width: 4 });
+    }
+
+    for (const bld of this.buildings) {
+      bld.draw(ctx);
     }
   }
 }
