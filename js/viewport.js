c4a40c17ef8cef85c819b2e589a6bf908a4607d4
Yadav106 <sahil.jp.yadav@gmail.com>
2024-11-06 21:46:46 +0530

    added stop markings

--- js/viewport.js
+++ js/viewport.js
@@ -3,7 +3,7 @@ class Viewport {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
 
-    this.zoom = 4;
+    this.zoom = 1;
     this.center = new Point(canvas.width / 2, canvas.height / 2);
     this.offset = scale(this.center, -1);
 
@@ -52,7 +52,7 @@ class Viewport {
     const step = 0.1;
     this.zoom += dir * step;
 
-    this.zoom = Math.max(1, Math.min(5, this.zoom));
+    this.zoom = Math.max(1, Math.min(10, this.zoom));
   }
 
   #handleMouseDown(evt) {
