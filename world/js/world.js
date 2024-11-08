f0ccf310e13e50cc72887e00133d84c5349b5e70
Yadav106 <sahil.jp.yadav@gmail.com>
2024-11-08 10:36:54 +0530

    added minimap

--- world/js/world.js
+++ world/js/world.js
@@ -270,7 +270,7 @@ class World {
     return bases.map(b => new Building(b));
   }
 
-  draw(ctx, viewPoint, showStartMarkings = true) {
+  draw(ctx, viewPoint, showStartMarkings = true, renderRadius = 1000) {
     this.#updateLights();
     for (const env of this.envelopes) {
       env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
@@ -300,7 +300,9 @@ class World {
       this.bestCar.draw(ctx, true);
     }
 
-    const items = [...this.buildings, ...this.trees];
+    const items = [...this.buildings, ...this.trees].filter(
+      (i) => i.base.distanceToPoint(viewPoint) < renderRadius
+    );
 
     items.sort((a, b) => 
       b.base.distanceToPoint(viewPoint) - a.base.distanceToPoint(viewPoint)
